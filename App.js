340d280c7bafbb60c7e844396c0a84da285fadd9
wcoelho <wcoelho@esec.com.br>
2021-07-30 22:32:51 -0300

    Esqueleto do projeto

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+// Disciplina Desenvolvimento de Aplicativos - UnB - CIC - Brasilia - Brasil
+// Andrey Emmanuel Matrosov Mazépas - Bruno Ribeiro das Virgens - William Coelho da Silva 
+
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
@@ -5,7 +8,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello World, DevApps</Text>
       <StatusBar style="auto" />
     </View>
   );
