77dc4002fafb002a84d5b317a2fe4fa24972313d
Andrey Mazépas <andreymazepas@gmail.com>
2021-08-05 22:49:02 -0300

    dark theme support?

--- App.jsx
+++ App.jsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { View, Text, Button } from "react-native";
+import { View, Text, Button, useColorScheme } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
@@ -16,9 +16,20 @@ function HomeScreen({ navigation }) {
 }
 
 function DetailsScreen() {
+  const colourScheme = useColorScheme();
+  const isDarkMode = colourScheme === "dark";
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Details Screen</Text>
+    <View
+      style={{
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: isDarkMode ? "black" : "white",
+      }}
+    >
+      <Text style={{ color: isDarkMode ? "white" : "black" }}>
+        Details Screen {colourScheme}
+      </Text>
     </View>
   );
 }
