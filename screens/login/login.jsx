579dad07f7f673f2bf1667385720de7cfa002c00
wcoelho <wcoelho@esec.com.br>
2021-08-23 21:18:52 -0300

    Removendo cadastro de dados no banco no login

--- screens/login/login.jsx
+++ screens/login/login.jsx
@@ -16,24 +16,6 @@ const Login = ({navigation}) => {
       firebase.auth().signInWithEmailAndPassword(email, password)
       .then(() => {
         console.log('User account created & signed in!');
-        const email64 = new Buffer(email).toString('base64')
-        navigation.navigate('Home')
-        dbUser.collection("users").doc(email64).set({
-          email: email,
-          fullName: "",
-          age: "",
-          state: "",
-          city: "",
-          anddress: "",
-          phone: "",
-          userName: "",
-        })
-        .then((docRef) => {
-            console.log("Document written with ID: ", docRef.id);
-        })
-        .catch((error) => {
-            console.error("Error adding document: ", error);
-        });
       })
       .catch(error => {
         console.log('ERROR')
