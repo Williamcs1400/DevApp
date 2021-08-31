05ac6b8707afabb4ef68f01147d6de9bd4911067
Andrey Mazépas <andreymazepas@gmail.com>
2021-08-30 21:13:08 -0300

    add image picker

--- components/ImagePicker/index.jsx
+++ components/ImagePicker/index.jsx
@@ -1,22 +1,50 @@
-import React from 'react';
-import {View} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {View, Platform, Image} from 'react-native';
 import PropTypes from 'prop-types';
 import {Card, useTheme} from 'react-native-paper';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import {Ionicons} from '@expo/vector-icons';
+import * as ExpoImagePicker from 'expo-image-picker';
+import I18n from 'i18n-js';
 import Text from '../Text';
 import Label from '../Label';
-import I18n from 'i18n-js';
 
 const ImagePicker = ({label}) => {
   const {colors} = useTheme();
+  const [image, setImage] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      if (Platform.OS !== 'web') {
+        const {status} = await ExpoImagePicker.requestMediaLibraryPermissionsAsync();
+        if (status !== 'granted') {
+          alert('Sorry, we need camera roll permissions to make this work!');
+        }
+      }
+    })();
+  }, []);
+
+  const pickImage = async () => {
+    const result = await ExpoImagePicker.launchImageLibraryAsync({
+      mediaTypes: ExpoImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    console.log(result);
+
+    if (!result.cancelled) {
+      setImage(result.uri);
+    }
+  };
 
   return (
     <View style={{padding: 16}}>
       {label && <Label name={label} />}
       <Card
         elevation={3}
-        onPress={() => {}}
+        onPress={pickImage}
         theme={{roundness: 0}}
         style={{
           height: 200,
@@ -26,10 +54,16 @@ const ImagePicker = ({label}) => {
         }}
       >
         <Card.Content style={{justifyContent: 'center', alignItems: 'center', flex: 1}}>
-          <Ionicons name="add-circle-outline" size={32} color={colors.iconColor} />
-          <Text color={colors.iconColor} fontSize={20}>
-            {I18n.t('addPicture')}
-          </Text>
+          {image ? (
+            <Image source={{uri: image}} style={{width: 200, height: 200}} />
+          ) : (
+            <>
+              <Ionicons name="add-circle-outline" size={32} color={colors.iconColor} />
+              <Text color={colors.iconColor} fontSize={20}>
+                {I18n.t('addPicture')}
+              </Text>
+            </>
+          )}
         </Card.Content>
       </Card>
     </View>
