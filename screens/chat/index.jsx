6ae51d0d759f4c21641ec174b03620a10d15a3df
Andrey Mazépas <andreymazepas@gmail.com>
2021-10-25 19:08:21 -0300

    ChatList working

--- screens/chat/index.jsx
+++ screens/chat/index.jsx
@@ -1,5 +1,5 @@
 import React, {createRef, useState, useEffect} from 'react';
-import {View, ScrollView, Text} from 'react-native';
+import {View, ScrollView, Text, BackHandler} from 'react-native';
 import {Button} from 'react-native-paper';
 import firebase from 'firebase';
 import {TextInput} from '../../components';
@@ -10,6 +10,7 @@ const MessageBox = ({message}) => (
 
 const Chat = ({route, navigation}) => {
   const [messages, setMessages] = useState([]);
+  const [chatId, setChatId] = useState('');
   const email64 = new Buffer(firebase.auth().currentUser.email).toString('base64');
 
   // pegar o campo chats do user
@@ -19,9 +20,10 @@ const Chat = ({route, navigation}) => {
   const recipientDocRef = firebase.firestore().collection('users').doc(route.params.user);
   const fetchChat = async () => {
     const docSnap = await docRef.get();
-    const existingChat = docSnap.data().chats.find((chat) => {
-      chat.user === route.params.user;
-    });
+    const existingChat = docSnap
+      .data()
+      .chats.find((chat) => chat.user === route.params.user);
+    console.log(existingChat);
 
     if (existingChat) {
       // pegar chat já existente com o usuario
@@ -31,10 +33,13 @@ const Chat = ({route, navigation}) => {
         .doc(existingChat.id.replace(/\s/g, ''))
         .get();
       console.log(chat.data().messages);
+      setChatId(existingChat.id.replace(/\s/g, ''));
       setMessages(chat.data().messages);
     } else {
       // criar chat com a pessoa
       // pegar o id gerado do chat e adicionar o chat no perfil dos dois usuarios
+
+      console.log('deu ruim');
       const newChat = {
         last: {
           author: '',
@@ -48,6 +53,7 @@ const Chat = ({route, navigation}) => {
         },
       };
       const res = await firebase.firestore().collection('chats').add(newChat);
+      setChatId(res.id.replace(/\s/g, ''));
       setMessages([{author: 'eu', message: res.id}]);
 
       docRef.update({
@@ -68,15 +74,29 @@ const Chat = ({route, navigation}) => {
 
   useEffect(() => {
     fetchChat();
+    const backHandler = BackHandler.addEventListener('hardwareBackPress', () => {
+      navigation.navigate('ChatList');
+    });
+    return () => backHandler.remove();
   }, []);
 
   const [message, setMessage] = useState('');
   const ref = createRef();
   const sendMessage = () => {
     if (message) {
-      messages.push({author: 'eu', message});
+      // messages.push({author: 'eu', message});
+      const messageObj = {
+        author: email64,
+        message,
+        timestamp: Date.now(),
+      };
       setMessage('');
       ref.current.clear();
+      const chatRef = firebase.firestore().collection('chats').doc(chatId);
+      chatRef.update({
+        last: messageObj,
+        messages: firebase.firestore.FieldValue.arrayUnion(messageObj),
+      });
     }
   };
 
--- screens/chatList/index.jsx
+++ screens/chatList/index.jsx
@@ -1,12 +1,93 @@
-import React from 'react';
-import {View, ScrollView, Text} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {View, ScrollView} from 'react-native';
+import firebase from 'firebase';
+import {Avatar, Card} from 'react-native-paper';
+import {TouchableOpacity} from 'react-native-gesture-handler';
+import {Text} from '../../components';
 
-const ChatList = ({route, navigation}) => (
-  <View style={{flex: 1}}>
-    <ScrollView contentContainerStyle={{padding: 8}}>
-      <Text>chats aparecerão aqui</Text>
-    </ScrollView>
-  </View>
-);
+const getInitials = (fullName) => {
+  let initials = fullName.split(' ') || [];
+
+  initials = ((initials.shift()?.[0] || '') + (initials.pop()?.[0] || '')).toUpperCase();
+
+  return initials;
+};
+
+const Chat = ({chatObj, onPressCallback}) => {
+  const email64 = new Buffer(firebase.auth().currentUser.email).toString('base64');
+
+  return (
+    <TouchableOpacity onPress={onPressCallback}>
+      <Card.Title
+        title={chatObj.user.name}
+        subtitle={`${chatObj.lastMessage.author === email64 ? 'Enviado:' : ''} ${
+          chatObj.lastMessage.message.length > 25
+            ? `${chatObj.lastMessage.message.substring(0, 25)}...`
+            : chatObj.lastMessage.message
+        }`}
+        left={() => <Avatar.Text size={42} label={getInitials(chatObj.user.name)} />}
+        right={() => (
+          <Text fontWeight="light">{`${new Date(
+            chatObj.lastMessage.timestamp,
+          ).getHours()}:${new Date(chatObj.lastMessage.timestamp).getMinutes()}`}</Text>
+        )}
+      />
+    </TouchableOpacity>
+  );
+};
+const ChatList = ({route, navigation}) => {
+  const email64 = new Buffer(firebase.auth().currentUser.email).toString('base64');
+
+  const [chatList, setChatList] = useState([]);
+
+  const fetchChatList = async () => {
+    // pegar o campo chats do user
+    // acessar o chat com id certo
+
+    const docRef = firebase.firestore().collection('users').doc(email64);
+    const docSnap = await docRef.get();
+    const chatListArray = docSnap.data().chats.map(async (chat) => {
+      const user = await firebase.firestore().collection('users').doc(chat.user).get();
+      const lastMessage = await firebase
+        .firestore()
+        .collection('chats')
+        .doc(chat.id.replace(/\s/g, ''))
+        .get('last');
+
+      const chatObj = {
+        id: chat.id,
+        user: {
+          id: chat.user,
+          name: user.data().fullName,
+        },
+        lastMessage: lastMessage.data().last,
+      };
+
+      return chatObj;
+    });
+
+    Promise.all(chatListArray).then((resolved) => {
+      setChatList(resolved);
+      console.log(resolved);
+    });
+  };
+
+  useEffect(() => {
+    fetchChatList();
+  }, []);
+  return (
+    <View style={{flex: 1}}>
+      <ScrollView contentContainerStyle={{padding: 8}}>
+        {chatList.map((c) => (
+          <Chat
+            chatObj={c}
+            key={c.id}
+            onPressCallback={() => navigation.navigate('Chat', {user: c.user.id})}
+          />
+        ))}
+      </ScrollView>
+    </View>
+  );
+};
 
 export default ChatList;
